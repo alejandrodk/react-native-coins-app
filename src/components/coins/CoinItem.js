86d8553288241804cc6fc1a012944166f5755607
alejandrodk <ottoabarriosp@hotmail.com>
2021-04-21 19:07:19 -0300

    Update items style

--- src/components/coins/CoinItem.js
+++ src/components/coins/CoinItem.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 const CoinItem = ({item}) => (
-  <View>
-    <Text>{item.name}</Text>
-    <Text>{item.symbol}</Text>
+  <View style={styles.container}>
+    <View style={styles.row}>
+      <Text style={styles.symbolText}>{item.symbol}</Text>
+      <Text style={styles.nameText}>{item.name}</Text>
+      <Text style={styles.priceText}>{`$${item.price_usd}`}</Text>
+    </View>
+
+    <View style={styles.row}>
+      <Text style={styles.percentText}>{item.percent_change_1h}</Text>
+    </View>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  symbolText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginRight: 12,
+  },
+  nameText: {
+    color: '#FFF',
+    fontSize: 14,
+    marginRight: 16,
+  },
+  percentText: {
+    color: '#FFF',
+    fontSize: 12,
+  },
+  priceText: {
+    color: '#FFF',
+    fontSize: 14,
+  },
+});
 
 export default CoinItem;
