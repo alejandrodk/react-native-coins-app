36760cab27ecf6169a91cbfadf70f3949faf01d0
alejandrodk <ottoabarriosp@hotmail.com>
2021-04-21 20:44:06 -0300

    Create section list

--- src/components/coins/CoinDetailScreen.js
+++ src/components/coins/CoinDetailScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, SectionList} from 'react-native';
 
 import Colors from '../../resources/colors';
 
@@ -13,6 +13,21 @@ const CoinDetailScreen = ({route, navigation}) => {
       return `https://c1.coinlore.com/img/16x16/${coinNameId}.png`;
   };
 
+  const getSections = coin => [
+    {
+      title: 'Market cap',
+      data: [coin.market_cap_usd],
+    },
+    {
+      title: 'Volume 24h',
+      data: [coin.volume24],
+    },
+    {
+      title: 'Change 24h',
+      data: [coin.percent_change_24h],
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <View style={styles.subHeader}>
@@ -22,6 +37,21 @@ const CoinDetailScreen = ({route, navigation}) => {
         />
         <Text style={styles.titleText}>{coin.name}</Text>
       </View>
+      <SectionList
+        style={styles.sectionHeader}
+        sections={getSections(coin)}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <View style={styles.sectionItem}>
+            <Text style={styles.itemText}>{item}</Text>
+          </View>
+        )}
+        renderSectionHeader={({section}) => (
+          <View style={styles.sectionHeader}>
+            <Text style={styles.sectionText}>{section.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -32,7 +62,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   subHeader: {
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
     padding: 16,
     flexDirection: 'row',
   },
@@ -44,6 +74,22 @@ const styles = StyleSheet.create({
   iconImg: {
     width: 25,
     height: 25,
+  },
+  sectionHeader: {
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    padding: 8,
+  },
+  sectionItem: {
+    padding: 8,
+  },
+  itemText: {
+    color: '#FFF',
+    fontSize: 14,
+  },
+  sectionText: {
+    color: '#FFF',
+    fontSize: 14,
+    fontWeight: 'bold',
   },
 });
 
